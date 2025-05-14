714c27f6cd6fa7f46947662588e012738a0e67a8
zoebowe <162049423+zoebowe@users.noreply.github.com>
2025-05-14 14:58:54 -0700

    Connect homepage to DB and insert 12 dynamic products

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('store.db');
 
 router.get('/', (req, res) => {
-  res.render('homepage'); // matches views/homepage.pug
+  db.all('SELECT * FROM products', [], (err, rows) => {
+    if (err) {
+      return res.status(500).send("Database error");
+    }
+    res.render('homepage', { products: rows });
+  });
 });
 
 module.exports = router;
