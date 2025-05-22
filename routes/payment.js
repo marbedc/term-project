4cee489391d64761db5d34c6359894f37b5402a7
marbedc <marbellacarrasco324@gmail.com>
2025-05-21 21:07:27 -0700

    fixing errors

--- routes/payment.js
+++ routes/payment.js
@@ -1,30 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const db = require('../db.js');
-
-const current_user = 2;
-
-router.get('/', (req, res) => {
-    
-    // Get the user's information from the database
-    db.get('SELECT * FROM users WHERE id = ?', [current_user], (err, user) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Internal Server Error');
-        }
-        if (!user) {
-            return res.status(404).send('User not found');
-        }else{
-            console.log(user);
-            res.render('payment');
-        }
-    
-    });
-
-});
-
-=======
 const db = require('../db');
 
 router.get('/payment', (req, res) => {
@@ -122,5 +97,4 @@ router.post('/payment', (req, res) => {
 });
 
 
->>>>>>> Marbella's-finished-work
 module.exports = router;
